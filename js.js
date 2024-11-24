b31078c7370367160cc09b6f6f6e356037b75dad
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-11-24 15:59:47 +0300

    h

---
+++ js.js
@@ -0,0 +1,45 @@
+
+//1
+
+const friends = ["Mango", "Poly", "Kivs", "Ajax"];
+
+
+
+const newfriends = friends.join(",")
+console.log(newfriends)
+
+//2
+
+const cards = [
+    "Карточка-1",
+    "Карточка-2",
+    "Карточка-3",
+    "Карточка-4",
+    "Карточка-5",
+  ];
+
+  console.log(cards);
+
+  //3
+
+  const cardToRemove = ["1", "2", "3", "4", "5", "6"];
+  const card = cardToRemove.splice(2, 1)
+
+console.log(card);
+
+
+// 4
+
+const cardss = ["1", "2", "3", "4", "5"];
+const plus = cardss.push("6")
+
+console.log(cardss)
+
+//5
+
+
+
+
+
+
+
